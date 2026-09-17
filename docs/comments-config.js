9f35a7919a9e1e1d8b0643d38221d98893e887d1
Gzaraki <112765179+clienteg1603@users.noreply.github.com>
2026-09-17 09:47:36 -0300

    Adicionar configuração dos comentários

---
+++ docs/comments-config.js
@@ -0,0 +1,7 @@
+// Configuração pública da área de comentários do site.
+// Estes valores serão preenchidos após a conexão com o serviço de comentários.
+window.GPH_COMMENTS = {
+  submitUrl: "",
+  listUrl: "",
+  publicHeaders: {}
+};
