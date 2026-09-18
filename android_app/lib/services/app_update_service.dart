5b1faead80898228f0abf620632bd90a1e72a4e8
Gzaraki <112765179+clienteg1603@users.noreply.github.com>
2026-09-18 19:46:27 -0300

    Android Alpha 11: atualiza identificador do app

--- android_app/lib/services/app_update_service.dart
+++ android_app/lib/services/app_update_service.dart
@@ -21,8 +21,8 @@ class AppUpdateInfo {
 class AppUpdateService {
   AppUpdateService({http.Client? client}) : _client = client ?? http.Client();
 
-  static const int currentBuild = 10;
-  static const String currentVersion = '0.1.7 alpha 10';
+  static const int currentBuild = 11;
+  static const String currentVersion = '0.1.8 alpha 11';
   static const String manifestUrl =
       'https://raw.githubusercontent.com/clienteg1603/GP-H-Consulta-Historica/android-release/latest.json';
 
