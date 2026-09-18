ac303ed9cb57d59018a3266020762593f4e8ecd2
Gzaraki <112765179+clienteg1603@users.noreply.github.com>
2026-09-18 17:13:09 -0300

    Android Alpha 6: atualiza versão do canal

--- android_app/lib/services/app_update_service.dart
+++ android_app/lib/services/app_update_service.dart
@@ -21,8 +21,8 @@ class AppUpdateInfo {
 class AppUpdateService {
   AppUpdateService({http.Client? client}) : _client = client ?? http.Client();
 
-  static const int currentBuild = 5;
-  static const String currentVersion = '0.1.2 alpha 5';
+  static const int currentBuild = 6;
+  static const String currentVersion = '0.1.3 alpha 6';
   static const String manifestUrl =
       'https://raw.githubusercontent.com/clienteg1603/GP-H-Consulta-Historica/android-release/latest.json';
 
