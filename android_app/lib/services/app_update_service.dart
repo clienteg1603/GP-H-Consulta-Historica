73ba717301fde673c159a340f50d034a0fff441d
Gzaraki <112765179+clienteg1603@users.noreply.github.com>
2026-09-19 07:45:03 -0300

    Android Alpha 20: corrige build interno do atualizador

--- android_app/lib/services/app_update_service.dart
+++ android_app/lib/services/app_update_service.dart
@@ -21,8 +21,8 @@ class AppUpdateInfo {
 class AppUpdateService {
   AppUpdateService({http.Client? client}) : _client = client ?? http.Client();
 
-  static const int currentBuild = 18;
-  static const String currentVersion = '0.1.15 alpha 18';
+  static const int currentBuild = 20;
+  static const String currentVersion = '0.1.17 alpha 20';
   static const String manifestUrl =
       'https://raw.githubusercontent.com/clienteg1603/GP-H-Consulta-Historica/android-release/latest.json';
 
